b1e62cb13ba955230581782291f6b85b791bb9b2
Sergey <dingilevskiys@gmail.com>
2023-01-25 22:58:55 +0300

    Task#1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 const app = express()
 
@@ -7,32 +8,65 @@ const port = 3000
 app.listen(port, () => console.log('Server is Runing'))
 
 const users = [ 
-  { id: 1, name: 'Sergey' },
-  { id: 5, name: 'Julianna' },
-  { id: 4, name: 'Daniil' },
-  { id: 3, name: 'Pasha' },
-  { id: 2, name: 'Denis' }
+  { id: 1, name: 'Sergey', isMan: true , age: 31 },
+  { id: 5, name: 'Julianna', isMan: false , age: 28 },
+  { id: 4, name: 'Daniil', isMan: true , age: 1 },
+  { id: 3, name: 'Pasha', isMan: true , age: 42 },
+  { id: 2, name: 'Denis', isMan: true , age: 31 },
+  { id: 6, name: 'Piter', isMan: true, age: 56}
 ]
 
-const jsonParser = bodyParser.json()
 
-app.get( '/user', (req, res) => res.send(users))
+// get all users
+app.get( '/users', (req, res) => res.send(users))
+
+//get user gender #male #female
+app.get( '/users/:gender', (req, res) => {
+  const isMan = req.params.gender
+  if ( isMan === "male" ) res.send( users.filter( i => i.isMan == true))
+  if ( isMan === "female" ) res.send( users.filter( i => i.isMan == false))
+})
+
+// get user age #min #max
+app.get( '/users', jsonParser, (req, res) => {
+  console.log(req.query)
+  console.log(JSON.stringify(req.query))
+  console.log(req.route)
+  const minAge = req.query.min
+  const maxAge = req.query.max
+  const filterAge = users.filter( i => i.age >= minAge && i.age <= maxAge ? i.age : false)
+  res.send(filterAge)
+})
+
+// add new user { id: ... , name: '...', isMan: boolean , age: ...  }
 app.post( '/user', jsonParser, (req, res) => {
-
   console.log(req.body)
   users.push(req.body)
   res.status(201)
-  res.send(users)
+  res.send(users.at(-1))
 })
 
+
+// refresh user all pametrs { id: ... , name: '...', isMan: boolean , age: ...  }
 app.put( '/user/:id',jsonParser, (req, res)=>{
   console.log(`${req.params.id}: ${req.body}`);
-  const updateUsers = users.map( i => ( i.id == req.params.id ? req.body : i ))
+  const id = req.params.id
+  const updateUsers = users.map( i => ( i.id == id ? req.body : i ))
   console.log(updateUsers);
   users.splice(0, users.length, ...updateUsers)
-  res.send(users)
+  res.send(users.filter( i => i.id == id))
 })
 
+// refresh user pametr
+app.patch( '/user/:id', jsonParser, (req, res) => {
+  const id = req.params.id
+  const updUser = req.body
+  const createUsers = users.filter( i => ( i.id == id ))
+  const createUser = Object.assign(...createUsers, updUser)
+  res.send(createUser)
+})
+
+// delete user { id: ... }
 app.delete( '/user/:id', (req, res) => {
   console.log(req.params.id)
   const id = users.findIndex( i => i.id === req.params.id)

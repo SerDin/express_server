ece3bb3c04845766c634b1cafcf0b7a50d9fd9cc
Sergey <dingilevskiys@gmail.com>
2023-02-02 23:31:44 +0300

    putUser - no fixed

--- services/user.services.js
+++ services/user.services.js
@@ -1,3 +1,4 @@
+const { all } = require('axios');
 const fs = require('fs');
 
 class UsersService {
@@ -35,52 +36,41 @@ class UsersService {
 
 	async putUser(data, id) {
 		//читаю файл с данными
-		await fs.readFile('data.json', 'utf8', (err, datas) => {
+		const put = await fs.readFile('data.json', 'utf8', async (err, datas) => {
 			if (err) throw err;
-			const users = JSON.parse(datas);
-			console.log(users);
-			const updateUsers = users.users.map((i) => (i.id == id ? data : i));
-			console.log('updateUsers', updateUsers);
+			const allUsers = JSON.parse(datas);
+			console.log('allUsers', allUsers);
+			const us = allUsers.users;
+			const putUser = us.map((i) => (i.id == id ? data : i));
+			console.log('data', data);
+			console.log('putUser', putUser);
 
-			const createdUser = updateUsers.splice(
-				0,
-				users.users.length,
-				...updateUsers
-			);
-			console.log('createdUser', createdUser);
-			const dataWrite = JSON.stringify(updateUsers);
-			console.log(typeof updateUsers);
+			await putUser.splice(0, putUser.length, {
+				users: [...putUser],
+			});
+			console.log('putUser', putUser[0]);
+			const dataWrite = JSON.stringify(putUser[0]);
+			console.log('dataWrite', dataWrite);
+			console.log('typeof putUser', typeof putUser);
 
-			fs.writeFile('data.json', dataWrite, function (err) {
+			return await fs.writeFile('data.json', dataWrite, function (err) {
 				if (err) {
 					throw err;
-				} else {
-					console.log('writeOk');
 				}
+				console.log('writeOk');
+				return fs.readFile('data.json', 'utf8', async (err, datas) => {
+					if (err) throw err;
+					const allUsers = await JSON.parse(datas);
+					console.log(
+						'allUsers',
+						allUsers.users.find((i) => i.id == id)
+					);
+					return allUsers.users.find((i) => i.id == id);
+				});
 			});
 		});
-
-		// const users = await this.readData()
-		// console.log(users);
-		// console.log('data, id',data, id);
-		// const updateUsers =  await users.map( i => i.id == id ? data : i )
-		// console.log('updateUsers',updateUsers);
-		// const createdUser = updateUsers.splice(0, users.length, ...updateUsers)
-		// console.log('createdUser',createdUser);
-		// //передал данные в запись
-		//  await this.writeData(updateUsers)
-		// //читаю файл с данными
-		// // const users2 = await this.readData()
-
-		// // console.log('users2',users2);
-		// // return  users2.filter( i => i.id == id)
-
-		// // return users.filter( i => i.id == id)
-		// 		const users2 = await this.readData()
-		// 		console.log(users2);
-
-		const users2 = await this.getUsersById(2);
-		return users2;
+		await console.log('put', put);
+		return await put;
 	}
 
 	async postUser(data) {
@@ -99,15 +89,6 @@ class UsersService {
 		const dataParse = await JSON.parse(datas);
 
 		return dataParse.users.at(-1);
-
-		// const users2 = await this.readData()
-
-		// return users2.users.at(-1)
-
-		// 	// return new Promise( (res,rej) => {
-		// 	// 	users.push(data)
-		// 	// 	// res.status(201)
-		// 	// })
 	}
 
 	patchUser(data) {
